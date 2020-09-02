d674ba2bdb5eef1a73fffd30f54d49c3f8b51870
vziks <vziks@live.ru>
2020-09-02 13:28:44 +0300

    (update) spring boot router function example

--- src/main/java/info/vziks/api/ApiApplication.java
+++ src/main/java/info/vziks/api/ApiApplication.java
@@ -2,8 +2,13 @@ package info.vziks.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,6 +17,21 @@ public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+    
+    
+    @Bean
+    RouterFunction<?> routerFunction() {
+        return RouterFunctions.route().GET("/route/one", this::createRoute).build()
+                .and(RouterFunctions.route().GET("/route/two", this::createRoute).build())
+                .and(RouterFunctions.route().GET("/route/three", this::createRoute).build());
+    }
+
+    private ServerResponse createRoute(ServerRequest serverRequest) {
+
+        ServerResponse serverResponse = ServerResponse.ok().render("create");
+
+        return serverResponse;
     }
 
 }
