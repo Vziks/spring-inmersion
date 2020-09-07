61c7b0808f64136eac0e2feb435c44c7a5e50c16
vziks <vziks@live.ru>
2020-09-07 14:26:01 +0300

    (update) spring i18n support by ApplicationContext

---
+++ src/main/java/info/vziks/api/test/i18nBeans.java
@@ -0,0 +1,31 @@
+package info.vziks.api.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Locale;
+
+/**
+ * Class i18nBeans
+ * Project spring-api
+ *
+ * @author Anton Prokhorov <vziks@live.ru>
+ */
+public class i18nBeans {
+
+
+    public static void main(String[] args) {
+
+        ApplicationContext cxt = new ClassPathXmlApplicationContext("classpath:myAppContext.xml");
+
+        Locale english = Locale.ENGLISH;
+        Locale russian = new Locale("ru", "RU");
+
+        System.out.println(cxt.getMessage("message", null, english));
+        System.out.println(cxt.getMessage("message", null, russian));
+        System.out.println(cxt.getMessage("messageParams", new Object[] {"code"}, english));
+        System.out.println(cxt.getMessage("messageParams", new Object[] {"code"}, russian));
+
+    }
+}
